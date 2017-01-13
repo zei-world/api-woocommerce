013b9fa8e58c0f0e5d8cae28434fc879eb8f294c
Nazim Lachter <lachtater@gmail.com>
2017-01-13 11:17:33 +0100

    Factorisation de la requete, ajout des listes

--- WooCommerce/zei-wc/includes/ZEI_WC_API.php
+++ WooCommerce/zei-wc/includes/ZEI_WC_API.php
@@ -15,17 +15,31 @@ class ZEI_WC_API {
 
     private $timeout = 2;
 
-    public function requestToken($id, $secret) {
-        $request = json_decode(file_get_contents($this->api.'token', false, stream_context_create([
-            'http'=>[
-                'method' => "GET", 'timeout' => $this->timeout,
-                'header' => "id: ".$id."\r\nsecret: ".$secret."\r\n"]
+    private function request($url, $header) {
+        return json_decode(file_get_contents($this->api.$url, false, stream_context_create([
+            'http' => [ 'method' => "GET", 'timeout' => $this->timeout, 'header' => $header ]
         ])), true);
+    }
+
+    public function requestToken($id, $secret) {
+        $request = $this->request('token', "id: ".$id."\r\nsecret: ".$secret."\r\n");
         if($request && $request['success'] && $request['token']) {
             $this->token = $request['token'];
             return true;
         }
         return false;
+    }
+
+    public function getOffersList() {
+        $request = $this->request('company/offers', "token: ".$this->token."\r\n");
+        if($request && $request['success'] && $request['message']) return $request['message'];
+        return null;
+    }
+
+    public function getRewardsList() {
+        $request = $this->request('company/rewards', "token: ".$this->token."\r\n");
+        if($request && $request['success'] && $request['message']) return $request['message'];
+        return null;
     }
 }
 
