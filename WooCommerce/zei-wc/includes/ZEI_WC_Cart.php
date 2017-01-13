a6f89e0250429bc5b296a27e9c64edbc285f07bf
Nazim Lachter <lachtater@gmail.com>
2017-01-13 14:41:56 +0100

    Ajout du module avec masquage automatique

--- WooCommerce/zei-wc/includes/ZEI_WC_Cart.php
+++ WooCommerce/zei-wc/includes/ZEI_WC_Cart.php
@@ -11,14 +11,25 @@ if(!class_exists('ZEI_WC_Cart')):
 
 class ZEI_WC_Cart {
     public function __construct() {
-        add_action('woocommerce_after_cart_table', array($this, "module"));
+        add_action('woocommerce_before_cart_totals', array($this, "module"));
     }
 
     public function module() {
+        global $woocommerce;
         if(isset($_SESSION['zeiToken'])) {
-            include_once 'ZEI_WC_API.php';
-            $url = ZEI_WC_API::getModuleUrl($_SESSION['zeiToken'], true, true);
-            if($url) echo "<object id=\"ZEI\" width=\"360px\" height=\"60px\" data=\"".$url."\"></object>";
+            $display = false;
+            foreach($woocommerce->cart->get_cart() as $elem) {
+                if(get_post_meta($elem['product_id'], 'zei_offer', true)) {
+                    $display = true;
+                    break;
+                }
+            }
+            if($display) {
+                include_once 'ZEI_WC_API.php';
+                $url = ZEI_WC_API::getModuleUrl($_SESSION['zeiToken'], true, true);
+                if($url) echo "<object id=\"ZEI\" width=\"360px\" height=\"60px\" data=\""
+                    .$url."\"></object>";
+            }
         }
     }
 }
