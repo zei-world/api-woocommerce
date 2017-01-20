4a7e0ed8bca7c46a681e00d9c0d5bf2af19d4a15
Nazim Lachter <lachtater@gmail.com>
2017-01-20 16:20:21 +0100

    Utiliser Curl s'il est installé

--- WooCommerce/zei-wc/includes/ZEI_WC_API.php
+++ WooCommerce/zei-wc/includes/ZEI_WC_API.php
@@ -12,15 +12,30 @@ if(!class_exists('ZEI_WC_API')):
 class ZEI_WC_API {
     private static $api = "https://zero-ecoimpact.org/api/";
 
-    private static function request($url, $header) {
-        return json_decode(file_get_contents(self::$api.$url, false, stream_context_create([
-            'http' => [ 'method' => "GET", 'timeout' => 2, 'header' => $header ],
-            'ssl' => [ "verify_peer" => false, "verify_peer_name" => false ]
-        ])), true);
+    private static function request($url, $headers) {
+        $url = self::$api.$url;
+        if(function_exists('curl_version')) {
+            $header = [];
+            foreach($headers as $k => $v) array_push($header, $k.": ".$v);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            $response = curl_exec($ch);
+            curl_close($ch);
+        } else {
+            $header = "";
+            foreach($headers as $k => $v) $header .= $k.": ".$v."\r\n";
+            $response = file_get_contents($url, false, stream_context_create([
+                'http' => [ 'method' => "GET", 'timeout' => 2, 'header' => $header ],
+                'ssl' => [ "verify_peer" => false, "verify_peer_name" => false ]
+            ]));
+        }
+        return json_decode($response, true);
     }
 
     public static function requestToken($id, $secret) {
-        $request = self::request('token', "id: ".$id."\r\nsecret: ".$secret."\r\n");
+        $request = self::request('token', ['id' => $id, 'secret' => $secret]);
         if($request && $request['success'] && $request['token']) return $request['token'];
         return null;
     }
@@ -40,14 +55,14 @@ class ZEI_WC_API {
     }
 
     public static function getOffersList($token) {
-        $request = self::request('company/offers', "token: ".$token."\r\n");
+        $request = self::request('company/offers', ['token' => $token]);
         if($request['message'] == '[OFFERS] Token has been used or not exist') self::getToken(false, true);
         if($request && $request['success'] && $request['message']) return $request['message'];
         return null;
     }
 
     public static function getRewardsList($token) {
-        $request = self::request('company/rewards', "token: ".$token."\r\n");
+        $request = self::request('company/rewards', ['token' => $token]);
         if($request['message'] == '[REWARDS] Token has been used or not exist') self::getToken(false, true);
         if($request && $request['success'] && $request['message']) return $request['message'];
         return null;
@@ -64,17 +79,17 @@ class ZEI_WC_API {
     }
 
     public static function validateOffer($token, $offerId, $amount) {
-        $r = self::request('company/offer', "token: ".$token."\r\noffer: ".$offerId."\r\namount: ".$amount."\r\n");
+        $r = self::request('company/offer', ['token' => $token, 'offer' => $offerId, 'amount' => $amount]);
         if($r['message'] == '[OFFER] Token has been used or not exist') self::getToken(false, true);
     }
 
-    public static function validateReward($token, $rewardId, $amount = null) {
-        $r = self::request('company/reward', "token: ".$token."\r\nreward: ".$rewardId."\r\namount: ".$amount."\r\n");
+    public static function validateReward($token, $rewardId, $amount) {
+        $r = self::request('company/reward', ['token' => $token, 'reward' => $rewardId, 'amount' => $amount]);
         if($r['message'] == '[REWARD] Token has been used or not exist') self::getToken(false, true);
     }
 
     public static function codesValidate($code) {
-        $request = self::request('company/codes', "token: ".self::getToken(false)."\r\ncode: ".$code."\r\n");
+        $request = self::request('company/codes', ['token' => self::getToken(false), 'code' => $code]);
         if($request['message'] == '[CODES] Token has been used or not exist') self::getToken(false, true);
         if($request && $request['success'] && $request['message']) return $request['message'];
         return null;
